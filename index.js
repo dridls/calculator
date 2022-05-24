a73f963a6f5c34ab976588ba76511223dc146ede
Adriana Sampaio <adrianadelimasampaio@gmail.com>
2022-05-24 11:01:26 +0200

    add handleNumbers

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ let previousOperator;
 
 const screen = document.querySelector(".screen");
 
-function buttonClick() {}
+function buttonClick(value) {
+  if (isNaN(value)) {
+    handleSign(value);
+  } else {
+    handleNumber(value);
+  }
+}
+
+const handleSign = (sign) => {};
+
+const handleNumber = (numberString) => {
+  if (buffer === "0") {
+    buffer = numberString;
+  } else {
+    buffer = buffer + numberString;
+  }
+  console.log(buffer);
+};
 
 function init() {
   document
     .querySelector(".calc-buttons")
     .addEventListener("click", function (event) {
+      console.log(event);
       buttonClick(event.target.innerText);
     });
 }
